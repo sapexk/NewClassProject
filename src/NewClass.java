ef0306cbd3b672d20a703a69a01380cc9f900e12
sapexk <52297254+sapexk@users.noreply.github.com>
2019-09-17 20:31:31 -0500

    Update NewClass.java

--- src/NewClass.java
+++ src/NewClass.java
@@ -2,6 +2,8 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
-       
+       System.out.println("68");
+        System.out.println("10");
+        System.out.println("12");
     }
 }
